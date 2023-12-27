69480290489e4f081df833ce28ef38b39f4c92aa
mateoruiz5171 <mateoruiz5171@gmail.com>
2023-12-27 15:41:18 -0800

    fixed input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@
 
 import React, { useState, useEffect } from 'react';
 import Navbar from '@/app/_components/navbar'
-import { VStack, Box, Text, Button, Input, Flex } from '@chakra-ui/react';
+import { VStack, Box, Text, Button, Input, Flex, InputGroup, InputRightElement } from '@chakra-ui/react';
 import LoadingScreen from './_components/loading';
+import { ArrowUpIcon } from '@chakra-ui/icons';
+
+
 
 export default function Home() {
   const [message, setMessage] = useState('');
@@ -18,7 +21,7 @@ export default function Home() {
       text: "Hello!"
     }
   ]
-  const [profileLoaded, setProfileLoaded] = useState(false);
+  const [profileLoaded, setProfileLoaded] = useState(true);
 
   if (!profileLoaded) {
     return <LoadingScreen />;
@@ -27,17 +30,23 @@ export default function Home() {
 return (
   <main className="bg-[#191414] h-screen">
     <Navbar/>
-    <Flex direction="column" justifyContent="space-between" h="calc(100vh - 10vh)" p={4}>
+    <Flex direction="column" justifyContent="space-between" width="75%" marginRight="auto" marginLeft="auto" h="calc(100vh - 10vh)" p={4}>
       <VStack spacing={4} align="stretch">
         {messages.map((message, index) => (
-          <Box key={index} alignSelf={message.sender === "user" ? "flex-end" : "flex-start"}>
+          <Box key={index} alignSelf="flex-start">
             <Text color={message.sender === "user" ? "blue.500" : "green.500"}>{message.text}</Text>
           </Box>
         ))}
       </VStack>
-      <Box display="flex" alignItems="center" mt={4} width="75%" marginLeft="auto" marginRight="auto">
-        <Input value={input} color="white" onChange={e=> setInput(e.target.value)} placeholder="Type a message" />
-        <Button ml={2} colorScheme="teal">Send</Button>
+      <Box display="flex" alignItems="center" mt={4}>
+        <InputGroup size="md" alignItems="center">
+          <Input pr="4.5rem" type="text" placeholder="Type a message" value={input} onChange={e=> setInput(e.target.value)} height="2.5rem" p="1.5rem"/>
+          <InputRightElement p="1.5rem">
+            <Button size="sm" colorScheme="teal">
+              <ArrowUpIcon />
+            </Button>
+          </InputRightElement>
+        </InputGroup>
       </Box>
     </Flex>
   </main>
